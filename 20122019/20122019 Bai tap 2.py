4c6a42699a5c13faa9dff2ef6f6c3d80df4f6746
sonvt19 <vutungson310@gmail.com>
2019-12-27 16:44:17 +0700

    temp

--- 20122019/20122019 Bai tap 2.py
+++ 20122019/20122019 Bai tap 2.py
@@ -1,8 +1,9 @@
 import math
+
 print("Tinh chu vi, dien tich hinh chu nhat ---------------")
 a = input("Nhap chieu dai: ")
 b = input("Nhap chieu rong: ")
-chuvi = 2*(float(a) + float(b))
+chuvi = 2 * (float(a) + float(b))
 dientich = float(a) * float(b)
 print("Tinh chu vi hinh chu nhat: " + str(chuvi))
 print("Tinh dien tich hinh chu nhat: " + str(dientich))
@@ -12,7 +13,7 @@ a = input("Nhap canh 1: ")
 b = input("Nhap canh 2: ")
 c = input("Nhap canh 3: ")
 chuvi = float(a) + float(b) + float(c)
-p = chuvi/2
+p = chuvi / 2
 dientich = math.sqrt(p * (p - float(a)) * (p - float(b)) * (p - float(c)))
 print("Tinh chu vi hinh tam giac: " + str(chuvi))
 print("Tinh dien tich hinh tam giac: " + str(dientich))
@@ -20,7 +21,7 @@ print("Tinh dien tich hinh tam giac: " + str(dientich))
 print("Tinh chu vi, dien tich hinh hinh tron ---------------")
 a = input("Nhap ban kinh: ")
 chuvi = float(a) * 2 * 3.14
+
 dientich = float(a) * float(a) * 3.14
 print("Tinh chu vi hinh tron: " + str(chuvi))
 print("Tinh dien tich hinh tron: " + str(dientich))
-
